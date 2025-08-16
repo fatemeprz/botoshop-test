6a7ce03bf84afb092926ca030b1a15dc8922845d
fateme parviz <fatemeprz1999@gmail.com>
2025-08-16 21:28:09 +0330

    Clear old build files

--- src/constants/list.js
+++
@@ -1,9 +0,0 @@
-  const categoryList = [
-    "All",
-    "Electronics",
-    "Jewelery",
-    "Men's Clothing",
-    "women's Clothing",
-  ];
-
-  export {categoryList}
--- src/helpers/shorterName.js
+++
@@ -1,6 +0,0 @@
-const shorterName = (name) => {
-  const result = name.split(" ").splice(0, 4).join(" ")
-  return result
-};
-
-export {shorterName}
