64dd28dab5a8b2a4e2748831a8bb173004da3f30
fateme parviz <fatemeprz1999@gmail.com>
2025-09-08 20:24:04 +0330

    refactor: initialize Redux Toolkit store and slice (start migrating from Context API)

---
+++ src/features/productsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+
+const fetchProducts = createAsyncThunk("products/fetchProducts", () => {
+  return axios.get("/products");
+});
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      (state.loading = false),
+        (state.data = action.payload),
+        (state.error = "");
+    });
+    builder.addCase(fetchProducts.rejected, (state, action) => {
+      (state.loading = false),
+        (state.data = []),
+        (state.error = action.error.message);
+    });
+  },
+});
+
+export default productsSlice.reducer;
+export { fetchProducts };
