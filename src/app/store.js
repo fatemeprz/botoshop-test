36a6213b2482b29055fc629f88988c84fef1b986
fateme parviz <fatemeprz1999@gmail.com>
2025-09-25 14:25:48 +0330

    logger middleware was deleted

--- src/app/store.js
+++ src/app/store.js
@@ -1,27 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "../features/product/productsSlice"
-import cartReducer from "../features/cart/cartSlice"
+import productsReducer from "../features/product/productsSlice";
+import cartReducer from "../features/cart/cartSlice";
 import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { persistReducer } from "redux-persist";
 import persistStore from "redux-persist/lib/persistStore";
-import { FLUSH,REGISTER,REHYDRATE,PAUSE,PERSIST,PURGE } from "redux-persist";
+import {
+  FLUSH,
+  REGISTER,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+} from "redux-persist";
 
-const persistConfig={
-    key:"cart",
-    storage,
-    blacklist:["loading"]
-}
+const persistConfig = {
+  key: "cart",
+  storage,
+  blacklist: ["loading"],
+};
 
-const persistCartReducer=persistReducer(persistConfig,cartReducer)
+const persistCartReducer = persistReducer(persistConfig, cartReducer);
 
-const store=configureStore({
-    reducer:{products:productsReducer,cart:persistCartReducer},
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-        serializableCheck:{ignoreActions:[REGISTER,PERSIST,PAUSE,PURGE,FLUSH,REHYDRATE]}
-    }).concat(logger)
-})
+const store = configureStore({
+  reducer: { products: productsReducer, cart: persistCartReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [REGISTER, PERSIST, PAUSE, PURGE, FLUSH, REHYDRATE],
+      },
+    }),
+});
 
-const persistor=persistStore(store)
+const persistor = persistStore(store);
 
-export {store,persistor}
+export { store, persistor };
